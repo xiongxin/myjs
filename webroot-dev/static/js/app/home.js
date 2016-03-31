bedfd2a3fae4706d0a4530b139672e040245ccdc
熊鑫 <1781189926@qq.com>
2016-03-31 17:49:07 +0800

    first commit

---
+++ webroot-dev/static/js/app/home.js
@@ -0,0 +1,9 @@
+/**
+ * 描述:
+ * Created by xiongxin on 2016/3/31.
+ */
+define(function (require, exports, module) {
+    'use strict';
+    var klass = require('../modules/klass');
+    klass._$klass('ok!');
+});
